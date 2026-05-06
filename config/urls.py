26540efad692b159db422cefd0c5c97c0b1027c3
reeba <iamneeba@gmail.com>
2026-05-06 13:25:29 +0500

    chore: configure Unfold, simple-history, rangefilter, and admin-confirm

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('challan/', include('apps.challans.urls', namespace='challans')),
+] 
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOTs)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
